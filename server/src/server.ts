40902e2ca4a7d8f01e342eb7cf00906dc21e3f25
kaushik-24 <grg23kaushik@gmail.com>
2025-07-11 20:04:55 +0545

    optimization

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import { DotenvConfig } from "./config/env.config";
 import Print from "./utils/print";
 
 function listen() {
-    const PORT = DotenvConfig.PORT;
+    const PORT = DotenvConfig.PORT || 3000;
     const httpServer = createServer(app);
     httpServer.listen(PORT); 
     Print.info(`🚀 Server is listening on port ${PORT}`);
@@ -14,6 +14,8 @@ function listen() {
 mongoose
     .connect(DotenvConfig.DATABASE_HOST as string, {
         serverSelectionTimeoutMS: 5000, // Timeout after 5s
+        maxPoolSize: 5, // Limit to 5 connections
+        bufferCommands: false, // Disable Mongoose buffering
     })
     .then(() => {
         listen();
