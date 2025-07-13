23121ad82326a45a3265c53848fbef19ca82c46e
kaushik-24 <grg23kaushik@gmail.com>
2025-07-13 10:05:59 +0545

    adjust deployment

--- server/src/config/app.config.ts
+++ server/src/config/app.config.ts
@@ -5,11 +5,6 @@ import middleware from "../middleware/index.middleware";
 const app = express();
 
 app.use((req, res, next) => {
-    if (req.path === '/api/auth/register' || req.path === '/api/auth/login') {
-        return next();
-    }
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) return res.status(403).json({ error: 'No token provided' });
     res.locals.sanitizeHtml = sanitizeHtml;
     next();
 });
